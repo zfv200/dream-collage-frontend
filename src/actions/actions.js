8aaac8b775484ef72632c739600be8e30d3444d7
github username <github email address>
2018-05-17 17:48:45 -0400

    last commit for the day, function uncommented

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,42 +2,42 @@
 // const headers = { "Content-Type":"application/json"}
 
 
-// export function imageSearch(phrase){
-//   return (dispatch) => {
-//     fetch(`https://cors-anywhere.herokuapp.com/https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=${phrase}`, {
-//       headers: {
-//         "Content-Type":"application/json",
-//         "Accept":"application/json",
-//         'Ocp-Apim-Subscription-Key':'derp'
-//       },
-//       method: 'get'
-//     })
-//     .then(res=>res.json())
-//     .then(json=>{
-//       dispatch(addImage(json.value[0]['contentUrl']))
-//     })
-//   }
-// }
-//
-// export function analysisSearch(content){
-//   return (dispatch) => {
-//     fetch("https://cors-anywhere.herokuapp.com/https://api.rosette.com/rest/v1/topics", {
-//       headers: {
-//         'X-RosetteAPI-Key':'derp',
-//         'Content-Type':'application/json',
-//         'Accept':'application/json',
-//       },
-//       method: "POST",
-//       body: JSON.stringify({"content":content})
-//     })
-//     .then(res=>res.json())
-//     .then(json=>{
-//       if (json['keyphrases'].length > 0){
-//         dispatch(addAnalysis(json))
-//       }
-//     })
-//   }
-// }
+export function imageSearch(phrase){
+  return (dispatch) => {
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=${phrase}`, {
+      headers: {
+        "Content-Type":"application/json",
+        "Accept":"application/json",
+        'Ocp-Apim-Subscription-Key':'derp'
+      },
+      method: 'get'
+    })
+    .then(res=>res.json())
+    .then(json=>{
+      dispatch(addImage(json.value[0]['contentUrl']))
+    })
+  }
+}
+
+export function analysisSearch(content){
+  return (dispatch) => {
+    fetch("https://cors-anywhere.herokuapp.com/https://api.rosette.com/rest/v1/topics", {
+      headers: {
+        'X-RosetteAPI-Key':'derp',
+        'Content-Type':'application/json',
+        'Accept':'application/json',
+      },
+      method: "POST",
+      body: JSON.stringify({"content":content})
+    })
+    .then(res=>res.json())
+    .then(json=>{
+      if (json['keyphrases'].length > 0){
+        dispatch(addAnalysis(json))
+      }
+    })
+  }
+}
 
 export function startDreaming(){
   return {
